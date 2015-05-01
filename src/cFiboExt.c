f9cebb99e49bdad68cbf8f7441003e4bcf0555cc
Ioannis Tziakos <ioannist@enthought.com>
2015-05-01 13:32:26 +0100

    better support for python2 and python 3 checks

--- src/cFiboExt.c
+++ src/cFiboExt.c
@@ -13,7 +13,11 @@ static PyObject *python_fibonacci(PyObject *module, PyObject *arg) {
     PyObject *ret = NULL;
     assert(arg);
     Py_INCREF(arg);
+#if PY_MAJOR_VERSION >= 3
+    if (!PyLong_Check(arg)) {
+#else
     if (!PyLong_Check(arg) & !PyInt_Check(arg)) {
+#endif
     	PyErr_SetString(PyExc_ValueError, "Argument is not an integer.");
     	goto except;
     }
